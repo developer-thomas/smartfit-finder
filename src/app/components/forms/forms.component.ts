fd5f2a4a03e22302d896822d74526012d4cfcedb
developer-thomas <andepipous@gmail.com>
2024-01-02 16:17:31 -0400

    feat: add form control with FormBuilder and FormControl

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss'],
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
   public results: string[] = [];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    });
+  }
+
+  onSubmit(): void {
+    console.log('submit');
+    console.log(this.formGroup.value);
+  }
+  onClean(): void {
+    console.log('clean');
+    this.formGroup.reset();
+  }
 }
